365accc8e0f0a86a8d49ccc6179db02a01197ff3
joschonarth <joschonarth@gmail.com>
2026-09-23 20:13:40 -0300

    test: implement unit tests for exercise service and setup workflow to run tests

--- src/test/java/br/com/joschonarth/springfit/SpringFitApplicationTests.java
+++ src/test/java/br/com/joschonarth/springfit/SpringFitApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.joschonarth.springfit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringFitApplicationTests {
 
 	@Test
